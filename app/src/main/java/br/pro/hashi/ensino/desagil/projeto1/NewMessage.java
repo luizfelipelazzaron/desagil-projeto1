b3ebcdfe3855a4043915e685b2c3173a40db1178
Enrico Damiani <enrico.francesco.damiani2gmail.com>
2020-05-04 09:37:31 -0300

    Interface mudada

--- app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/NewMessage.java
+++ app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/NewMessage.java
@@ -10,19 +10,20 @@ public class NewMessage extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_new_message);
 
 
-        Button button_new_message = findViewById(R.id.button_new_message);
+//        Button button_new_message = findViewById(R.id.button_new_message);
 
-        button_new_message.setOnClickListener((view -> {
-            startNewMSGActivity();
-        }));
+//        button_new_message.setOnClickListener((view -> {
+//            startNewMSGActivity();
+//        }));
 
-    }
-
-    private void startSMSActivity () {
-        
+//    }
+//
+//    private void startSMSActivity () {
+//
     }
 
 }
