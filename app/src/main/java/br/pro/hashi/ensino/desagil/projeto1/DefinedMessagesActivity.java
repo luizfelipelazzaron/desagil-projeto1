b24c5753e18017be740faf7cc49904b0cf483245
Enrico Damiani <enrico.francesco.damiani2gmail.com>
2020-05-05 19:47:05 -0300

    Segredo

--- app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/DefinedMessagesActivity.java
+++ app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/DefinedMessagesActivity.java
@@ -1,30 +1,26 @@
 package br.pro.hashi.ensino.desagil.projeto1;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.telephony.PhoneNumberUtils;
-import android.telephony.SmsManager;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class DefinedMessagesActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+public class DefinedMessagesActivity<Static> extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
-    // Método de conveniência para mostrar uma bolha de texto.
-    private void showToast(String text) {
+    static String  message;
+    TextView textView;
 
-        // Constrói uma bolha de duração curta.
-        Toast toast = Toast.makeText(this, text, Toast.LENGTH_SHORT);
+    static String getMessage(){
 
-        // Mostra essa bolha.
-        toast.show();
+        return message;
     }
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,46 +28,38 @@ public class DefinedMessagesActivity extends AppCompatActivity implements Adapte
         setContentView(R.layout.activity_defined_messages);
 
         // EditText textMessage = findViewById(R.id.text_message);
-        // EditText textPhone = findViewById(R.id.text_phone);
-        Button buttonSend = findViewById(R.id.button_send);
+        // EditText textPhone = findViewById(R.id.text_phone)
 
         Spinner spinner = findViewById(R.id.spinner_messages);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.messages, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
         spinner.setOnItemSelectedListener(this);
+        message = spinner.getSelectedItem().toString();
 
-        buttonSend.setOnClickListener((view) -> {
-            String message = spinner.getSelectedItem().toString();
+        if (savedInstanceState != null) {
+            message = savedInstanceState.getString("GAME_STATE_KEY");
+        }
 
-            if (message.isEmpty()) {
-                showToast("Mensagem inválida!");
-                return;
-            }
+        textView = (TextView) findViewById(R.id.text_view);
 
-           // String phone = textPhone.getText().toString();
-            String phone = "+5511988284562";
+        Button enviar = findViewById(R.id.enviar);
 
-            // Esta verificação do número de telefone é bem
-            // rígida, pois exige até mesmo o código do país.
-            if (!PhoneNumberUtils.isGlobalPhoneNumber(phone)) {
-                showToast("Número inválido!");
-                return;
-            } else {
-                showToast("Mensagem enviada");
-            }
+        enviar.setOnClickListener((view -> {
+            startSendMessageActivity();
+        }));
 
-            // Enviar uma mensagem de SMS. Por simplicidade,
-            // não estou verificando se foi mesmo enviada,
-            // mas é possível fazer uma versão que verifica.
-            SmsManager manager = SmsManager.getDefault();
-            manager.sendTextMessage(phone, null, message, null, null);
-
-            // Limpar o campo para nenhum engraçadinho
-            // ficar apertando o botão várias vezes.
-            // textPhone.setText("");
-        });
     }
+
+    public void onSaveInstanceState(Bundle outState){
+        outState.putString("GAME_STATE_KEY", message);
+
+    }
+
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        textView.setText(savedInstanceState.getString(TEXT_VIEW_KEY));
+    }
+
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
@@ -82,4 +70,11 @@ public class DefinedMessagesActivity extends AppCompatActivity implements Adapte
     @Override
     public void onNothingSelected(AdapterView<?> adapterView) {
     }
+
+    private void startSendMessageActivity() {
+        Intent intent = new Intent(this, SendMessage.class);
+        intent.putExtra("arg", this.message);
+        startActivity(intent);
+    }
+
 }
