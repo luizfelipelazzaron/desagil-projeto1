b5a5d5ec4d35fab64d24b7ceb1ff5fb1d869d56b
cicerotcv <49568618+cicerotcv@users.noreply.github.com>
2020-05-05 19:41:48 -0300

    modificações no translator

--- app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/Translator.java
+++ app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/Translator.java
@@ -16,13 +16,24 @@ public class Translator {
     // Você deve mudar o recheio do construtor,
     // de acordo com os requisitos do projeto.
     public Translator() {
+
+        this.map.put('a', new Node('-'));
         this.root = new Node(' ');
+        Node e = new Node('e');
+        Node t = new Node('t');
+        root.setLeft(e);
+        root.setRight(t);
+        e.setParent(root);
+        t.setParent(root);
+
+
         this.root.setLeft(new Node('e'));
         this.root.setRight(new Node('t'));
         this.root.getLeft().setLeft(new Node('i'));
         this.root.getLeft().setRight(new Node('a'));
         this.root.getRight().setLeft(new Node('n'));
         this.root.getRight().setRight(new Node('m'));
+
         this.root.getLeft().getLeft().setLeft(new Node('s'));
         this.root.getLeft().getLeft().setRight(new Node('u'));
         this.root.getLeft().getRight().setLeft(new Node('r'));
@@ -31,6 +42,7 @@ public class Translator {
         this.root.getRight().getLeft().setRight(new Node('k'));
         this.root.getRight().getRight().setLeft(new Node('g'));
         this.root.getRight().getRight().setRight(new Node('o'));
+
         this.root.getLeft().getLeft().getLeft().setLeft(new Node('h'));
         this.root.getLeft().getLeft().getLeft().setRight(new Node('v'));
         this.root.getLeft().getLeft().getRight().setLeft(new Node('f'));
@@ -43,6 +55,7 @@ public class Translator {
         this.root.getRight().getLeft().getRight().setRight(new Node('y'));
         this.root.getRight().getRight().getLeft().setLeft(new Node('z'));
         this.root.getRight().getRight().getLeft().setRight(new Node('q'));
+
         this.root.getLeft().getLeft().getLeft().getLeft().setLeft(new Node('5'));
         this.root.getLeft().getLeft().getLeft().getLeft().setRight(new Node('4'));
         this.root.getLeft().getLeft().getLeft().getRight().setRight(new Node('3'));
