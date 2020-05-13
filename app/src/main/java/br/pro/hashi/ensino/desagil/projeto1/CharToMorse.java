fb542aaa99cc9bcfc943b1da68ab83bf665574cb
cicerotcv <49568618+cicerotcv@users.noreply.github.com>
2020-05-13 02:29:14 -0300

    mais ou menos funcionando

---
+++ app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/CharToMorse.java
@@ -0,0 +1,14 @@
+package br.pro.hashi.ensino.desagil.projeto1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CharToMorse extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_char_to_morse);
+    }
+}
